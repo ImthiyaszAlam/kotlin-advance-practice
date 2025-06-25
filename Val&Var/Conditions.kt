31fdaffcf1400112ea5d7a4af9c71b0da2ed322d
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-25 13:36:13 +0530

    if-else-if

---
+++ Val&Var/Conditions.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val score = 80
+
+    if (score >= 80) {
+        println("gradeA")
+    } else if (score >= 60) {
+
+        println("gradeB")
+    } else {
+
+        println("gradeC")
+    }
+}
