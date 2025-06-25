7f035d4edabffea3f0eca54b8b685260ed712697
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-25 13:44:37 +0530

    when-self-task

---
+++ Val&Var/When2.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+
+    val day = 5
+
+    val plan = when(day){
+       in 1..5->"Coding"
+
+       6 , 7->"Chil"
+
+       else->"Nothing"
+
+    }
+
+    println("Today my plan is : $plan")
+}
