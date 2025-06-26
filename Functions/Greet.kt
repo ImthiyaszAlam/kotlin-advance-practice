017c96dea8f165bdf3ee22bfae3a4568f0230299
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-26 18:16:08 +0530

    fn with return type

--- Functions/Greet.kt
+++ Functions/Greet.kt
@@ -3,7 +3,18 @@ fun greet(name: String) {
     println("Good Morning $name")
 }
 
+fun add(a: Int, b: Int): Int {
+    val sum: Int = a + b
+
+    return sum
+}
+
 fun main() {
     val name = "Alam"
     greet(name)
+
+    val a = 5
+    val b = 3
+
+    println(add(a, b))
 }
