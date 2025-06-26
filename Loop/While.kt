3002ae587ece190a7e150f511011c168d288b7fe
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-26 18:22:24 +0530

    for-loop

---
+++ Loop/While.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val items = listOf("a", "b", "c", "d")
+
+    for (item in items) {
+        println("$item,")
+    }
+
+    println("")
+
+    for (i in 1..4) {
+        print("$i,")
+    }
+    // println(items)
+}
