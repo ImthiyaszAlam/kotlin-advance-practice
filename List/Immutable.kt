98fea58ae264842138f45598a869468fb79174fd
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-27 16:25:31 +0530

    list-immutable and mutable

---
+++ List/Immutable.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val languages = listOf("java", "python", "kotlin")
+    val names = mutableListOf("Ali", "Anna", "Alam")
+
+    println("$languages")
+
+    println(" $names")
+
+    names.add("AlamLearning")
+
+    println(" $names")
+
+
+}
