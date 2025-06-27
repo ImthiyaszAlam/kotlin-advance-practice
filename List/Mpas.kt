b63802ae09d6815637e0d0700cac10b4075f2987
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-27 16:36:25 +0530

    mapOf -indexing

--- List/Mpas.kt
+++ List/Mpas.kt
@@ -3,4 +3,8 @@ fun main() {
     val user = mapOf("name" to "Imthiyas", "city" to "Delhi", "age" to "24")
 
     println(" $user")
+
+    println("Name: ${user["name"]} ")
+    println("City: ${user["city"]}")
+    println("Age: ${user["age"]}")
 }
