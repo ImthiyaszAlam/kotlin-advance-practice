0d363e5cb0487d825bd4d0c0d0954b5dee3c7f0e
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-01 18:23:12 +0530

    map practice

--- Collections/Map.kt
+++ Collections/Map.kt
@@ -2,4 +2,8 @@ fun main() {
     val numbers = listOf(1, 2, 3, 4, 5)
     val squares = numbers.map { it * it }
     println("$squares")
+
+    val number = listOf(1, 2, 3, 4, 5)
+    val sum = number.map { it + it }
+    println(sum)
 }
