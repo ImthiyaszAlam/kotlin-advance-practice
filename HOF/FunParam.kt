f78be98d473bd2b8436f86cc2373353824337beb
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-02 14:48:26 +0530

    correction in params

--- HOF/FunParam.kt
+++ HOF/FunParam.kt
@@ -3,7 +3,7 @@ fun main() {
     val result = operate(5, 10) { a, b -> a + b }
     println(result)
 
-    val sub = subtract(20, 10) { x, y -> x - b }
+    val sub = subtract(20, 10) { x, y -> x - y }
     println(sub)
 }
 
@@ -11,6 +11,6 @@ fun operate(x: Int, y: Int, op: (Int, Int) -> Int): Int {
     return (op(x, y))
 }
 
-fun subtract(x: Int, y: Int, sub: (Int, Int) -> Int): Int {
-    return (sub(x, y))
+fun subtract(a: Int, b: Int, sub: (Int, Int) -> Int): Int {
+    return (sub(a, b))
 }
