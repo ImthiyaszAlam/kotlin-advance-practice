44bf9283933b614e3d861b4ba477d744ec9feee6
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-04 16:17:16 +0530

    extension function

---
+++ Null Safety/ExtensionFun.kt
@@ -0,0 +1,13 @@
+fun String.addHashTag(): String {
+    return "#$this"
+}
+
+
+
+fun
+
+fun main() {
+    val word = "Koltin"
+    val aadedHashTag = word.addHashTag()
+    println(aadedHashTag)
+}
