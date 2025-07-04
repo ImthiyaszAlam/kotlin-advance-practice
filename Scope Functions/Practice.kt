5d7c1780a8c2c268b18bd2aee2974ac4a3b9b052
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-04 15:55:09 +0530

    funs chaning

--- Scope Functions/Practice.kt
+++ Scope Functions/Practice.kt
@@ -1,5 +1,14 @@
 fun main() {
-    data class Developer(val name: String, var language: String)
+    data class Developer(var name: String, var language: String)
     val dev = Developer("Alam", "Kotlin")
     println(dev)
+    dev
+            .apply {
+                name = "Imthiyas"
+                language = "KotlinJava"
+            }
+            .also { println("Setup complete for $it") }
+            .run { println("Working with $language") }
+    val result = "Jetpack Compose".let { it.replace(" ", "_").uppercase() }
+    println(result)
 }
