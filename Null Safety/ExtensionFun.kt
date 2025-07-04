47de08ebf1cc283bd944d2401838818c537f8c64
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-04 16:27:11 +0530

    extension funs practice

--- Null Safety/ExtensionFun.kt
+++ Null Safety/ExtensionFun.kt
@@ -2,12 +2,32 @@ fun String.addHashTag(): String {
     return "#$this"
 }
 
+fun Int.addNumber(): Int {
+    return this + 123
+}
 
+fun Int.subNum(): Int {
+    return this - 2
+}
 
-fun
+fun Int.mulNum(): Int {
+    return this * this
+}
+
+fun Int.addNum(): String {
+    return this.toString() + "anna"
+}
 
 fun main() {
     val word = "Koltin"
     val aadedHashTag = word.addHashTag()
     println(aadedHashTag)
+
+    val num = 12
+
+    println(num.addNumber())
+
+    println(num.subNum())
+
+    println(num.mulNum())
 }
