e49cdc5ea23f5f2ed03e66c79a61f96e6df92825
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-04 16:07:25 +0530

    not null assertion

--- Null Safety/P1.kt
+++ Null Safety/P1.kt
@@ -2,13 +2,19 @@ fun main() {
     val name: String? = "Alam"
 
     val length = name?.length
-    println(length)
+
+    println("length $length")
 
     val language: String = "Kotlin"
-    val result = language?.substring(2)
-    println(result)
+    val result = language?.length
+
+    println("result $result")
 
     val input: String? = null
     val output = input ?: "Not empty"
-    println(output)
+    println("output $output")
+
+    val name1: String? = null
+    val length1 = name!!.length
+    println("length1 $length1")
 }
