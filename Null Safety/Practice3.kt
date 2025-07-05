f4326df3c6c5a71e767015a95651f66c797b2662
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-05 12:17:46 +0530

    run practice

---
+++ Null Safety/Practice3.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val grreting = "Morning Baby Girl"
+    val result =
+            grreting.run {
+                println(length)
+                uppercase()
+            }
+
+    println(grreting)
+}
