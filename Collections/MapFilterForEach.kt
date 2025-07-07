8fce30e9972e562f2fc5cece612982a40cdba446
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-07 16:07:41 +0530

    map

--- Collections/MapFilterForEach.kt
+++ Collections/MapFilterForEach.kt
@@ -2,5 +2,5 @@ fun main() {
 
     val numbers = mutableListOf(1, 2, 3, 4, 5, 6, 7)
 
-    numbers.forEach { it -> print(" $it th turn") }
+    numbers.map { it * 2 }.forEach { it -> println(" $it th turn") }
 }
