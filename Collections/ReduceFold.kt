7b8447d526ff6338cc87b91905ba11963454b902
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-08 11:23:14 +0530

    fold()

--- Collections/ReduceFold.kt
+++ Collections/ReduceFold.kt
@@ -1,5 +1,9 @@
 fun main() {
     val nums = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val result = nums.reduce { acc, i -> acc + i }
+    val result = nums.reduce { acc, i -> acc - i }
     println("Sum: $result")
+
+    val numbers = listOf(1, 2, 3)
+    val resultFold = numbers.fold(100) { a, i -> a - i }
+    println("resultFold: $resultFold")
 }
