9920897174c2a363113aca1e0eed9def5902b3e2
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-10 18:23:16 +0530

    concurrency

--- Coroutines/ThreadConcurrency.kt
+++ Coroutines/ThreadConcurrency.kt
@@ -1,3 +1,18 @@
-fun main(){
-    
+fun asyncTask(name: String) {
+    Thread {
+                println("$name : Task stated")
+                Thread.sleep(1000)
+                println("$name : Task finished")
+            }
+            .start()
+}
+
+fun main() {
+    println("Main starts")
+
+    asyncTask("task-1")
+    asyncTask("task-2")
+    println("Main ends (while tasks still running)")
+    Thread.sleep(2000)
+    println("After 2000")
 }
