cafd2ea759e2eb49b8d698317f096725dc87afc0
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-12 22:55:00 +0530

    TwoSumSorted

--- DSA Kotlin/TwoSum.kt
+++ DSA Kotlin/TwoSum.kt
@@ -2,7 +2,10 @@ fun main(){
 
 val arr = intArrayOf(1,2,3,4,5,6,7,8)
 var  target = 5;
-print(twoSumSorted(arr,target))
+var result = twoSumSorted(arr,target)
+println(result.joinToString())
+println(twoSumSorted(arr,target))
+
 }
 
 
@@ -22,4 +25,23 @@ fun twoSumSorted(arr:IntArray,target:Int):IntArray{
     }
 
     return intArrayOf(-1,-1)
+}
+
+
+
+fun twoSumSorted1(arr:IntArray,target:Int):IntArray{
+
+var l =0;
+var r = arr.size-1
+
+while(l<r){
+    var sum = arr[l] + arr[r]
+    when{
+        sum == target -> return intArrayOf(arr[l],arr[r])
+        sum<target -> l++
+        else -> r--;
+    }
+}
+
+return intArrayOf(-1,-1)
 }
