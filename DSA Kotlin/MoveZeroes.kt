e12bce4fb515c2a51a889f7c95a2c89ed43bb3ab
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-15 22:51:45 +0530

    Move zeroes using slow and fast pointer

--- DSA Kotlin/MoveZeroes.kt
+++ DSA Kotlin/MoveZeroes.kt
@@ -4,16 +4,21 @@
 
 fun main(){
 
+val array = intArrayOf(1,2,3,0,0,0,4,5,6,7,8,9)
+moveZeroes(array)
+println(array.joinToString())
 }
 
 
 fun moveZeroes(arr:IntArray){
-    var m = arr.size
+    var slow  = 0
 
-    for(i in 1 until m){
-        if(arr[i] ==0){
-            var temp= 0;
-        
+    for(fast in  arr.indices ){
+        if(arr[fast]!=0){
+            val temp = arr[slow]
+            arr[slow] = arr[fast]
+            arr[fast] = temp
+            slow++
         }
     }
 }
