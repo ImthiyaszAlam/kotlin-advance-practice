5cad2c2d52184dde8184fb1d2b0a3370c94a17e3
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-16 22:47:14 +0530

    Max Subarray Sum

--- DSA Kotlin/maxSumSubarray.kt
+++ DSA Kotlin/maxSumSubarray.kt
@@ -1,8 +1,25 @@
 fun main(){
 
+    val arr = intArrayOf(1,32,5,62,3,4,7,8)
+    val k = 4
+   
+    println( subArraySum(arr,k))
 }
 
 
 fun subArraySum(arr:IntArray,k:Int):Int{
     
+    var sum = 0
+    var max = Int.MIN_VALUE
+
+
+    for (i in arr.indices) {
+        sum+=arr[i]
+        if(i>=k-1){
+            max = maxOf(max,sum)
+            sum-=arr[i-k+1]
+        }
+    }
+
+    return max;
 }
