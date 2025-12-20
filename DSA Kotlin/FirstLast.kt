b423c4942dea51a7ad6c96bbfccd7356a4f06b69
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-20 21:38:16 +0530

    Find First & Last Index

--- DSA Kotlin/FirstLast.kt
+++ DSA Kotlin/FirstLast.kt
@@ -7,5 +7,21 @@ fun main(){
 
     fun find (isFirst:Boolean):Int{
 
+
+var l = 0;
+var r = arr.size-1;
+
+while(l<=r){
+    val  mid = l+(r-l)/2
+    when{
+        arr[mid] == target -> result = mid
+        if(isFirst){
+            r = mid-1
+        }else{
+            l = mid+1
+        }
+    }
+}
+
     }
  }
