9842f1a7757a4e9de17b8cf88f8a63d2ffe20238
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-30 22:41:57 +0530

    First Unique character

--- DSA Kotlin/String Hashing/FirstUniqueChar.kt
+++ DSA Kotlin/String Hashing/FirstUniqueChar.kt
@@ -1,18 +1,22 @@
-fun main(){
-
+fun main() {
+    val s = "aaggglam"
+    val result = firstUniqueChar(s)
+    print(result)
 }
 
+fun firstUniqueChar(s: String): Int {
+    val map = HashMap<Char, Int>()
 
-
-
-fun firstUniqueChar(s:String):Int{
-    val map = HashMap<Char,Int>
     for (c in s) {
-        map[c] = map.getOrDefault(c,0)+1
+        map[c] = map.getOrDefault(c, 0) + 1
     }
 
-    for(i in s.indices){
-        if(map[s[i]] == 1) retutn i;
+    for (i in s.indices) {
+        if (map[s[i]] == 1) {
+            print("char-->"+s[i] +" & index->")   
+            return i      
+        }
     }
-    retutn -1;
+
+    return -1
 }
