b0c802879fadd428a3a89a9b07c20add75796e3e
alam <developerimthiyas+imthiyas@gmail.com>
2026-01-14 22:44:48 +0530

    Backtracking

--- DSA Kotlin/BackTracking/Permute.kt
+++ DSA Kotlin/BackTracking/Permute.kt
@@ -7,9 +7,24 @@ fun permute(nums:IntArray):List<List<Int>>{
     val used = BooleanArray(nums.size)
 
     fun dfs(current:MutableListOf<Int>){
-        if(current.size = nums.size)
+        if(current.size = nums.size){
         result.add(ArrayList<current>)
         return
+        }
+
+        for (i in nums.indices) {
+            if(used[i] continue)
+            used[i] = true
+            current.add(nums[i])
+
+            dfs(current)
+
+            current.removeAt(current.size-1)
+            used[i] = false
+
+        }
     }
+    dfs(mutableListOf())
+    return res
 
 }
